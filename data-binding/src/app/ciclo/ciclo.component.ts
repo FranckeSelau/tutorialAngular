5df78cff621cbed09d8cfffdf09a04e8cd94d164
francke <francke.selau@gmail.com>
2018-02-07 18:46:19 -0200

    ciclo de vida component

---
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnInit {
+
+  constructor() { 
+    this.log('contrutor');
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
